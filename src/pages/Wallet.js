f9a5edab71c10eff672a0ffe11ab5cff8115df1d
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-11-27 03:59:53 -0300

    04, faltando ultimo req

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,14 +3,58 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expenses: [],
+    };
+  }
+
   render() {
     const { email } = this.props;
+
     return (
-      <header>
-        <h2 data-testid="email-field">{ email }</h2>
-        <h2 data-testid="total-field">0</h2>
-        <h2 data-testid="header-currency-field">BRL</h2>
-      </header>
+      <>
+        <header>
+          <h2 data-testid="email-field">{ email }</h2>
+          <h2 data-testid="total-field">0</h2>
+          <h2 data-testid="header-currency-field">BRL</h2>
+        </header>
+
+        <form>
+          <fieldset>
+            <input
+              data-testid="value-input"
+              label="Valor da despesa:"
+            />
+            <input
+              data-testid="description-input"
+              label="Descrição da despesa:"
+            />
+            <input
+              data-testid="currency-input"
+              label="Moeda da despesa:"
+            />
+
+            <select data-testid="method-input">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="creditCard">Cartão de crédito</option>
+              <option value="debitCard">Cartão de débito</option>
+            </select>
+
+            <select data-testid="tag-input">
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transport">Transporte</option>
+              <option value="health">Saúde</option>
+
+              <button type="submit">Adicionar despesa</button>
+            </select>
+          </fieldset>
+        </form>
+      </>
+
     );
   }
 }
